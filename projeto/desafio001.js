b3f663b44afb073d9710dbf5781ff811de0e7920
romersonrsantana <romersonsantana@hotmail.com.br>
2024-11-18 22:35:32 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- projeto/desafio001.js
+++ projeto/desafio001.js
@@ -1,1 +1,9 @@
+import express from "express";
+
+const app = express(); 
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+
 //Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
